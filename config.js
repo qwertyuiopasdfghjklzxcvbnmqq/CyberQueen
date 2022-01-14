d2c63686af5a1568d13b4e1f1f7e43efe052736c
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2022-01-14 11:22:26 +0530

    ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ

--- config.js
+++ config.js
@@ -41,6 +41,7 @@ module.exports = {
     SEND_READ: process.env.SEND_READ === undefined ? false : convertToBool(process.env.SEND_READ),
     ANTIGRP_LINK: process.env.ANTIGRP_LINK === undefined ? 'false' : process.env.ANTIGRP_LINK,
     RULES: process.env.RULES === undefined ? 'default' : process.env.RULES,
+    OWNER: process.env.OWNER === undefined ? 'default' : process.env.OWNER,
     RULES_LOGO: process.env.RULES_LOGO === undefined ? 'https://i.ibb.co/J3FK9zw/image.png' : process.env.RULES_LOGO,
     BRANCH: 'main',
     HEROKU: {
