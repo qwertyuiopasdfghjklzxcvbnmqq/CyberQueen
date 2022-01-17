825cef8114d49bec8fd9eb92849e2e21536b2c64
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2022-01-17 08:57:05 +0530

    ᳆⃞🇱🇰ᴄʏͥʙᴇͣʀͫ↯ǫᴇᴇɴᬐ

--- config.js
+++ config.js
@@ -42,6 +42,7 @@ module.exports = {
     ANTIGRP_LINK: process.env.ANTIGRP_LINK === undefined ? 'false' : process.env.ANTIGRP_LINK,
     RULES: process.env.RULES === undefined ? 'default' : process.env.RULES,
     OWNER: process.env.OWNER === undefined ? 'default' : process.env.OWNER,
+    ADDMSG: process.env.ADD_MSG === undefined ? 'default' : process.env.ADD_MSG,
     RULES_LOGO: process.env.RULES_LOGO === undefined ? 'https://i.ibb.co/J3FK9zw/image.png' : process.env.RULES_LOGO,
     BRANCH: 'main',
     HEROKU: {
